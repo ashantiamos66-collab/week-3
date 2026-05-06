2223cca7c701cdc4013de557394c21d8a93db08f
ashantiamos66-collab <ashantiamos66@gmail.com>
2026-05-06 16:44:57 -0500

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,51 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "myImage":return img`
+. . . . . f f f f . . . . . 
+. . . f f 5 5 5 5 f f . . . 
+. . f 5 5 5 5 5 5 5 5 f . . 
+. f 5 5 5 5 5 5 5 5 5 5 f . 
+. f 5 5 5 3 b b 3 5 5 5 f . 
+f 5 5 5 3 b b b b b 5 5 5 f 
+f 5 5 3 3 b b b b c c 5 5 f 
+f b b 3 8 8 b b 7 7 1 1 1 f 
+f b b 8 8 8 7 7 7 7 e 1 1 f 
+. f b 8 9 9 7 7 7 7 e 1 f . 
+. f 8 8 9 9 9 9 9 9 e e 1 . 
+. 8 8 8 9 9 9 9 9 6 e 4 e . 
+. 8 8 8 9 9 9 9 9 9 e d 4 . 
+. 8 8 8 8 8 8 8 8 e e f 4 . 
+. . 8 8 8 8 8 8 8 8 f f . . 
+. . . . 8 8 8 8 8 8 . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("json", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
